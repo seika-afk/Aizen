a28b665370f7e521430d631fffc104385313618f
Seika <artistnoob6@gmail.com>
2026-02-01 10:49:58 +0530

    addition : layout for the webpage

--- aizen/app/page.tsx
+++ aizen/app/page.tsx
@@ -1,10 +1,55 @@
-import Image from "next/image";
-
+'use client'
+import { useState,useEffect } from "react";
 export default function Home() {
-  return (
-     
-<div>
-Hello world
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const media = window.matchMedia('(max-width: 768px)');
+
+    const update = () => setIsMobile(media.matches);
+    update();
+
+    media.addEventListener('change', update);
+    return () => media.removeEventListener('change', update);
+  }, []);
+
+  if (isMobile === null) return null;
+if(isMobile) { 
+
+return(
+<div className="flex justify-center items-center h-screen">
+<p className="text-white">Currently not available on smaller devices. 
+	</p>
+	</div>
+)
+       }
+else{
+
+
+ 
+	return (
+
+
+
+<div className="flex  w-full h-screen ">
+<div className="bg-[#383D3F] flex items-center justify-center w-full h-full m-2 p-2 rounded-lg">
+  <div className="bg-[#313638] flex items-center justify-center w-full h-full p-2 rounded-lg">
+    <img id="src" className="w-full h-full object-contain" src="./file.svg" />
+  </div>
+</div>
+
+<div className="bg-[#383D3F] w-1/2 m-2 p-2 rounded-lg">
+right
+</div>
+
+
 </div>
   );
-}
+
+ }
+ 
+ 
+ }
+
+
+
